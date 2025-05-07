18cfaa8fc8b991016591ffd3459753db69d66053
LeunoraSeferi <ls30075@seeu.edu.mk>
2025-05-07 23:38:27 +0200

    Creating Product Repository

---
+++ DemoECommerce.ProductApiSolution/ProductApi.Infrastructure/DependencyInjection/ServiceContainer.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.SharedLibrary.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApi.Application.Interfaces;
+using ProductApi.Infrastructure.Data;
+using ProductApi.Infrastructure.Repositories;
+
+namespace ProductApi.Infrastructure.DependencyInjection
+{
+    public static class ServiceContainer
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration config)
+        {
+            // Add database connectivity
+            // Add authentication scheme
+            SharedServiceContainer.AddSharedServices<ProductDbContext>(services, config, config["MySerilog:FileName"]!);
+
+            // Create Dependency Injection (DI)
+            services.AddScoped<IProduct, ProductRepository>();
+
+            return services;
+        }
+        public static IApplicationBuilder UseInfrastrcturePolicy(this  IApplicationBuilder app) 
+        {
+            //register middleware such as:
+            //global exception:handles external errors
+            //listen to only Api Gateway:blocks all outsider calls;
+            SharedServiceContainer.UseSharedPolicies(app);
+            return app;
+
+           }
+    }
+}
