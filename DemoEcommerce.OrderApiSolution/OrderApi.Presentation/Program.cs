156ab262de704a60d8e3aa8fbe18269e3fc87327
LeunoraSeferi <ls30075@seeu.edu.mk>
2025-05-11 22:48:23 +0200

    defining Order Repository

--- DemoEcommerce.OrderApiSolution/OrderApi.Presentation/Program.cs
+++ DemoEcommerce.OrderApiSolution/OrderApi.Presentation/Program.cs
@@ -1,19 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using System.Globalization;
+using OrderApi.Infrastructure.DependencyInjection;
 
+var builder = WebApplication.CreateBuilder(args);
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddInfrastructureService(builder.Configuration);
 var app = builder.Build();
 
+app.UserInfrastructurePolicy();
 app.UseSwagger();
 app.UseSwaggerUI();
-
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
